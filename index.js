854bec5b61416b6e0ada5ffb06a6641076f685bc
Matt Born <mattborn@users.noreply.github.com>
2023-04-26 23:51:46 -0600

    From query to playable first result

--- index.js
+++ index.js
@@ -32,9 +32,28 @@ functions.http('tiktok', async (req, res) => {
           resource_type: 'video',
         })
         res.status(200).send(video)
-      } else {
+      } else if (endpoint === 'search') {
         const response = await tikapi.public.search(options)
         res.status(200).send(response?.json)
+      } else {
+        // this basically combines both operations above
+        const search = await tikapi.public.search(options)
+        const { id, video } = search?.json.item_list[0] // only the first result
+        const response = await tikapi.public.video({ id })
+        const videoData = await fetch(video.downloadAddr, {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+            ...response.json?.$other?.videoLinkHeaders,
+          },
+        })
+        const arrayBuffer = await videoData.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer).toString('base64')
+        const cloudVideo = await cloudinary.uploader.upload(`data:video/mp4;base64,${buffer}`, {
+          public_id: `videos/${id}`,
+          resource_type: 'video',
+        })
+        res.status(200).send(cloudVideo)
       }
     } catch (error) {
       res.status(500).send(error)
