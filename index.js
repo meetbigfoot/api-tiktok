7bef4ba589318a544558ffc0958fb32c1a9f9581
Matt Born <mattborn@users.noreply.github.com>
2023-04-26 23:16:43 -0600

    Hot fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ functions.http('tiktok', async (req, res) => {
   //cloud.google.com/functions/docs/samples/functions-http-cors#functions_http_cors-nodejs
   res.set('Access-Control-Allow-Origin', '*')
   if (req.method === 'OPTIONS') {
-    res.set('Access-Control-Allow-Methods', 'GET, POST')
+    res.set('Access-Control-Allow-Methods', 'POST')
     res.set('Access-Control-Allow-Headers', 'Content-Type')
     res.set('Access-Control-Max-Age', '3600')
     res.status(204).send('')
